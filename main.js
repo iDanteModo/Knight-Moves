a1225fdf707fcbb44ca6b148d3214153415eeb31
iDanteModo <76968864+iDanteModo@users.noreply.github.com>
2024-11-11 07:41:33 +0100

    LAtest

--- main.js
+++ main.js
@@ -8,13 +8,16 @@ class Graph {
     }
     addEdge(node1, node2) {
         this.adjacencyList.get(node1).add(node2);
-        this.adjacencyList.get(node2).add(node1);
+        this.adjacencyList.get(node2).add(node1); // remove this line for directed graph
     }
     createChessBoard() {
         for(let i = 0; i < 64; i++) {
             this.addNode(i);
         }
-        console.log(this.adjacencyList);
+        for(let j = 0; j < 63; j++) {
+            this.addEdge(j, j+1);
+        }
+        // console.log(this.adjacencyList);
     }
     getNeighbours(node) {
         console.log(this.adjacencyList.get(node));
@@ -25,12 +28,29 @@ class Graph {
     }
 }
 
+// For the knight pattern from node i to i +17 ori i +15 or i +10 or i + 6 / i -17 or i - 15 or i - 10 or i - 6 
+
+// function generateEdges(graph) {
+//     for(let i = 0; i < 63; i++) {
+//         graph.addEdge(i, i +1);
+//     }
+// }
+
 const list = new Graph();
 list.createChessBoard();
-list.addEdge(1,2);
-list.addEdge(1,4);
-list.getNeighbours(1);
+// list.getNeighbours(1);
+console.log(list);
 // console.log(list);
+
+
+
+
+
+
+
+
+
+
 // console.log(list.adjacencyList);
 // console.log(list.adjacencyList.entries().next().value); // acess first item from map
 // console.log([...list.adjacencyList.entries()][1]); // acess by index
